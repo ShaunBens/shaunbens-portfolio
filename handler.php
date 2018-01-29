a49e8f4b541a15633d18d6306aa1797799d5a182
Shaun Bens <bensshaun@gmail.com>
2018-01-29 00:40:59 +0000

    minor UI changes

--- handler.php
+++ handler.php
@@ -21,6 +21,6 @@ $validator->field('message')->maxLength(6000);
 
 
 
-$pp->sendEmailTo('shaun@shaunbens.com'); // ← Your email here
+$pp->sendEmailTo(['shaun@shaunbens.com', 'bensshaun@gmail.com']); // ← Your email here
 
 echo $pp->process($_POST);
